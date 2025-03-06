74e757deafce6e73a4ccab9326fff03c6da0ab73
Kauan P.A <148518067+kauan-passos-ataide@users.noreply.github.com>
2025-03-06 19:32:12 -0300

    update the system layout

--- src/components/modals/modal.tsx
+++ src/components/modals/modal.tsx
@@ -1,47 +1,69 @@
-'use client'
-import React, { useCallback, useEffect, useState } from 'react';
-import { DatePicker } from '../ui/date-picker';
-import { SubTodo, TodoList } from '@/types/TodoList';
-import { Trash2 } from 'lucide-react';
-import { Checkbox } from '../ui/checkbox';
-import SubModal from './sub modal';
+"use client";
+import React, { useCallback, useEffect, useState } from "react";
+import { DatePicker } from "../ui/date-picker";
+import { SubTodo, TodoList } from "@/types/TodoList";
+import { CornerUpLeft, Trash2 } from "lucide-react";
+import { Checkbox } from "../ui/checkbox";
+import SubModal from "./sub modal";
 
-const Modal = ({ show, onClose, onSubmit, todoForEdit, onDate }: 
-  { show: boolean; 
-    onClose: () => void; 
-    onAddNewTask: () => void; 
-    onSubmit: (title: string, date: Date | undefined, titleSubTodo: SubTodo[], idTodo?: number, deleteItem?: boolean) => void; 
-    todoForEdit?: TodoList; 
-    onDate?: Date}
-  ) => {
-
-  const [selectedDate, setSelectedDate] = useState<Date | undefined>(onDate || undefined);
+const Modal = ({
+  show,
+  onClose,
+  onSubmit,
+  todoForEdit,
+  onDate,
+}: {
+  show: boolean;
+  onClose: () => void;
+  onAddNewTask: () => void;
+  onSubmit: (
+    title: string,
+    date: Date | undefined,
+    titleSubTodo: SubTodo[],
+    idTodo?: number,
+    deleteItem?: boolean
+  ) => void;
+  todoForEdit?: TodoList;
+  onDate?: Date;
+}) => {
+  const [selectedDate, setSelectedDate] = useState<Date | undefined>(
+    onDate || undefined
+  );
   const [inputValue, setInputValue] = useState(todoForEdit?.label || "");
   const [showModal, setShowModal] = useState(false);
   const [titleToAdd, setTitleToAdd] = useState<string>("");
   const [shouldAddTask, setShouldAddTask] = useState<boolean>(false);
-  const [subTodoItem, setSubTodoItem] = useState<SubTodo[]>(todoForEdit?.subTodo || []);
+  const [subTodoItem, setSubTodoItem] = useState<SubTodo[]>(
+    todoForEdit?.subTodo || []
+  );
 
   const handleAddTask = useCallback(() => {
-      const newSubTodo = {
-          id: subTodoItem.length > 0 ? Math.max(...subTodoItem.map((item) => item.id)) + 1 : 1,
-          label: titleToAdd,
-          checked: false,
-        };
-        setSubTodoItem([...subTodoItem, newSubTodo]);
-    }, [titleToAdd, subTodoItem]);
+    const newSubTodo = {
+      id:
+        subTodoItem.length > 0
+          ? Math.max(...subTodoItem.map((item) => item.id)) + 1
+          : 1,
+      label: titleToAdd,
+      checked: false,
+    };
+    setSubTodoItem([...subTodoItem, newSubTodo]);
+  }, [titleToAdd, subTodoItem]);
 
   useEffect(() => {
-      if (shouldAddTask) {
-        handleAddTask();
-        setShouldAddTask(false); 
-      }
-    },[handleAddTask, shouldAddTask]);
-    
+    if (shouldAddTask) {
+      handleAddTask();
+      setShouldAddTask(false);
+    }
+  }, [handleAddTask, shouldAddTask]);
+
   if (!show) return null;
 
   const toggleItem = (itemIndex: number) => {
-    setSubTodoItem(subTodoItem.map((item) => item.id === itemIndex? {...item, checked: !item.checked} : item))
+    setSubTodoItem(
+      subTodoItem.map((item) =>
+        item.id === itemIndex ? { ...item, checked: !item.checked } : item
+      )
+    );
   };
 
   const handleChange = (title: string) => {
@@ -50,8 +72,10 @@ const Modal = ({ show, onClose, onSubmit, todoForEdit, onDate }:
   };
 
   const removeTask = (id: number) => {
-    setSubTodoItem(prevTodoItem => prevTodoItem.filter(item => item.id!== id));
-  }
+    setSubTodoItem((prevTodoItem) =>
+      prevTodoItem.filter((item) => item.id !== id)
+    );
+  };
 
   const handleSubmit = () => {
     if (!inputValue || !selectedDate) {
@@ -61,7 +85,7 @@ const Modal = ({ show, onClose, onSubmit, todoForEdit, onDate }:
     if (todoForEdit) {
       onSubmit(inputValue, selectedDate, subTodoItem, todoForEdit.id);
     } else {
-      onSubmit(inputValue, selectedDate, subTodoItem)
+      onSubmit(inputValue, selectedDate, subTodoItem);
     }
     onClose();
     setInputValue("");
@@ -78,85 +102,94 @@ const Modal = ({ show, onClose, onSubmit, todoForEdit, onDate }:
     setSubTodoItem([]);
   };
 
-
-
   return (
     <div className="fixed inset-0 w-screen h-screen bg-black bg-opacity-50 z-50 flex items-center justify-center">
-      <div className="bg-gray-100 w-11/12 lg:w-2/3 h-4/5 p-10 rounded-lg z-60 block items-center justify-center overflow-x-hidden overflow-y-auto">
-        <div className='flex flex-col gap-3 select-none'>
-          <div className='flex flex-row gap-5 select-none justify-between'>
-            <button onClick={onClose} className="px-4 py-2 border-2 border-red-600 text-red-600 rounded-md hover:text-white hover:bg-red-600 transition-all duration-300">
-              Fechar
-            </button>
-            <button onClick={handleSubmit} className="px-14 py-2 w-full bg-gradient-zinc bg-[length:200%_200%] animate-gradient-move text-white rounded flex justify-center items-center hover:bg-zinc-900 select-none">
+      <div className="bg-gray-100 w-11/12 lg:w-2/3 h-4/5 px-5 py-10 rounded-lg z-60 block items-center justify-center overflow-x-hidden overflow-y-auto">
+        <div className="flex flex-col gap-3 select-none">
+          <div className="flex flex-row gap-5 select-none">
+            <CornerUpLeft
+              onClick={onClose}
+              className="p-2 border-2 w-12 h-12 border-red-600 text-red-600 rounded-md hover:text-white hover:bg-red-600 transition-all duration-300"
+            />
+            <div>
+              {todoForEdit ? (
+                <Trash2
+                  onClick={handleDelete}
+                  className="p-2 border-2 w-12 h-12 border-red-600 text-red-600 rounded-md hover:text-white hover:bg-red-600 transition-all duration-300"
+                />
+              ) : (
+                []
+              )}
+            </div>
+            <button
+              onClick={handleSubmit}
+              className="px-14 py-2 bg-gradient-zinc bg-[length:200%_200%] animate-gradient-move text-white rounded flex justify-center items-center hover:bg-zinc-900 select-none"
+            >
               Salvar
             </button>
-          </div>
-          <div>
-            {todoForEdit? (
-              <button onClick={handleDelete} className="px-4 py-2 border-2 border-red-600 text-red-600 rounded-md hover:text-white hover:bg-red-600 transition-all duration-300">
-                <Trash2 />
-              </button>
-            )
-            : []}
           </div>
         </div>
         <div>
           <div className="mt-6 select-none">
             <span>Titulo</span>
           </div>
-          <div className='bg-gradient-white bg-[length:200%_200%] animate-gradient-move p-[1px] rounded-md'>
-            <input type="text" 
-            className='outline-none border rounded-md w-full h-9 p-2 text-zinc-800'
-            placeholder='Digite o título da tarefa'
-            value={inputValue}
-            onChange={(e) => setInputValue(e.target.value)}
+          <div className="bg-gradient-white bg-[length:200%_200%] animate-gradient-move p-[1px] rounded-md">
+            <input
+              type="text"
+              className="outline-none border rounded-md w-full h-9 p-2 text-zinc-800"
+              placeholder="Digite o título da tarefa"
+              value={inputValue}
+              onChange={(e) => setInputValue(e.target.value)}
             />
           </div>
           <div className="mt-6 select-none">
             <span>Data</span>
           </div>
-          <div className='select-none'>
-            <DatePicker 
+          <div className="select-none">
+            <DatePicker
               selectedDate={selectedDate}
               onDateChange={setSelectedDate}
             />
           </div>
         </div>
-        <div className='z-70 min-h-32 select-none'>
+        <div className="z-70 min-h-32 select-none">
           <div className="mt-6 flex flex-col gap-6">
-            <span>Sub tarefas</span>
-            <button 
-            onClick={() => setShowModal(true)} 
-            className='px-14 py-2 bg-gradient-zinc bg-[length:200%_200%] animate-gradient-move text-white rounded flex justify-center items-center hover:bg-zinc-900'
+            <span>Subtarefas</span>
+            <button
+              onClick={() => setShowModal(true)}
+              className="px-14 py-2 bg-gradient-zinc bg-[length:200%_200%] animate-gradient-move text-white rounded flex justify-center items-center hover:bg-zinc-900"
             >
-              Adicionar sub tarefa
+              Adicionar subtarefa
             </button>
             <SubModal
-            show={showModal} 
-            onClose={() => setShowModal(false)}
-            onSubmit={handleChange}
+              show={showModal}
+              onClose={() => setShowModal(false)}
+              onSubmit={handleChange}
             />
           </div>
-          <div className=''>
+          <div className="">
             {subTodoItem.map((item) => (
-                <div key={item.id}>
-                  <div className="py-2 bg-white mt-4 px-5 lg:px-14 flex justify-between border-b-2 rounded-b-xl border-zinc-200 cursor-pointer select-none">
-                    <div className='w-12 border-r border-zinc-300' onClick={() => removeTask(item.id)}>
-                      <Trash2 className='text-gray-800 w-4 hover:text-red-500'/>
-                    </div>
-                    <div className='w-full flex justify-start items-start px-5 text-zinc-700 whitespace-normal'>
-                      <span>{item.label}</span>
-                    </div>
-                    <div className='w-12 border-l border-zinc-300 flex justify-end items-end' onClick={() => toggleItem(item.id)}>
-                      <Checkbox checked={item.checked}/>
-                    </div>
+              <div key={item.id}>
+                <div className="py-2 bg-white mt-4 px-5 lg:px-14 flex justify-between border-b-2 rounded-b-xl border-zinc-200 cursor-pointer select-none">
+                  <div
+                    className="w-12 border-r border-zinc-300"
+                    onClick={() => removeTask(item.id)}
+                  >
+                    <Trash2 className="text-gray-800 w-4 hover:text-red-500" />
                   </div>
-                  <div className='w-full select-none'>
-                    
+                  <div className="w-full flex justify-start items-start px-5 text-zinc-700 whitespace-normal">
+                    <span>{item.label}</span>
+                  </div>
+                  <div
+                    className="w-12 border-l border-zinc-300 flex justify-end items-end"
+                    onClick={() => toggleItem(item.id)}
+                  >
+                    <Checkbox checked={item.checked} />
                   </div>
                 </div>
-              ))}
+                <div className="w-full select-none"></div>
+              </div>
+            ))}
           </div>
         </div>
       </div>
--- src/components/modals/sub modal.tsx
+++ src/components/modals/sub modal.tsx
@@ -1,9 +1,16 @@
-'use client'
-import React, { useState } from 'react';
+"use client";
+import React, { useState } from "react";
+import { CornerUpLeft } from "lucide-react";
 
-const SubModal = ({ show, onClose, onSubmit }: 
-  { show: boolean; onClose: () => void; onSubmit: (title: string) => void}) => {
-
+const SubModal = ({
+  show,
+  onClose,
+  onSubmit,
+}: {
+  show: boolean;
+  onClose: () => void;
+  onSubmit: (title: string) => void;
+}) => {
   const [inputValue, setInputValue] = useState("");
 
   if (!show) return null;
@@ -19,15 +26,18 @@ const SubModal = ({ show, onClose, onSubmit }:
     setInputValue("");
   };
 
-
   return (
     <div className="fixed inset-0 w-screen h-screen z-50 flex items-center justify-center select-none">
-      <div className="bg-gray-100 w-11/12 lg:w-2/3 h-4/5 p-10 rounded-lg z-60 block items-center justify-center overflow-x-hidden overflow-y-auto">
-        <div className='flex flex-row gap-5'>
-          <button onClick={onClose} className="px-4 py-2 border-2 border-red-600 text-red-600 rounded-md hover:text-white hover:bg-red-600 transition-all duration-300">
-            Fechar
-          </button>
-          <button onClick={handleSubmit} className="px-14 py-2 bg-gradient-zinc bg-[length:200%_200%] animate-gradient-move text-white rounded flex justify-center items-center hover:bg-zinc-900">
+      <div className="bg-gray-100 w-11/12 lg:w-2/3 h-4/5 px-5 py-10 rounded-lg z-60 block items-center justify-center overflow-x-hidden overflow-y-auto">
+        <div className="flex flex-row gap-5">
+          <CornerUpLeft
+            onClick={onClose}
+            className="p-2 border-2 w-12 h-12 border-red-600 text-red-600 rounded-md hover:text-white hover:bg-red-600 transition-all duration-300"
+          />
+          <button
+            onClick={handleSubmit}
+            className="px-14 py-2 bg-gradient-zinc bg-[length:200%_200%] animate-gradient-move text-white rounded flex justify-center items-center hover:bg-zinc-900"
+          >
             Salvar
           </button>
         </div>
@@ -35,18 +45,19 @@ const SubModal = ({ show, onClose, onSubmit }:
           <div className="mt-6">
             <span>Titulo</span>
           </div>
-          <div className='bg-gradient-white bg-[length:200%_200%] animate-gradient-move p-[1px] rounded-md'>
-            <input type="text" 
-            className='outline-none rounded-md w-full h-9 p-2 text-zinc-800 '
-            placeholder='Digite o título da sub tarefa'
-            value={inputValue}
-            onChange={(e) => setInputValue(e.target.value)}
+          <div className="bg-gradient-white bg-[length:200%_200%] animate-gradient-move p-[1px] rounded-md">
+            <input
+              type="text"
+              className="outline-none rounded-md w-full h-9 p-2 text-zinc-800 "
+              placeholder="Digite o título da subtarefa"
+              value={inputValue}
+              onChange={(e) => setInputValue(e.target.value)}
             />
           </div>
         </div>
       </div>
     </div>
-    );
-  };
+  );
+};
 
 export default SubModal;
